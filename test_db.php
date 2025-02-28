c07de9bb3181fd2bc04cdcac4b6af88f244bfac0
Infyinfy <125547553+2002cactus@users.noreply.github.com>
2025-03-01 01:55:22 +0700

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -5,7 +5,7 @@ $dbname = getenv('DB_NAME');
 $user = getenv('DB_USER');
 $pass = getenv('DB_PASS');
 
-$conn = pg_connect("host=$host dbname=$dbname user=$user password=$pass");
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$pass");
 
 if (!$conn) {
     die("Lỗi kết nối: " . pg_last_error());
